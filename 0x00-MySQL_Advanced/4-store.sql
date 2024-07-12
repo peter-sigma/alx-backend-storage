9cca5c1e892d3230b1577b84cc049c9ae07c8602
peter-sigma <petergichehacrypto@gmail.com>
2024-07-12 02:52:15 +0000

    5. Email validation to sent

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+--Create a trigger that decreases the quantity
+
+CREATE TRIGGER decrement
+AFTER INSERT
+ON orders
+FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
